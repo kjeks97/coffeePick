74e03071d7df9b61d3ff576b965b1ab7d906a063
ske97 <kjeks97@gmail.com>
2023-08-31 15:31:42 +0900

    second commit

---
+++ src/main/java/com/coffeePick/CoffeePickProjectApplication.java
@@ -0,0 +1,13 @@
+package com.coffeePick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeePickProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeePickProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coffeePick/CoffeePickProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeePick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeePickProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
